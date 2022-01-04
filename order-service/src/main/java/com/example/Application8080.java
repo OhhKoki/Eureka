ed25b20900334e6bd08500454af0bc07aef4d29b
terry <180319071@qq.com>
2022-01-04 18:32:00 +0800

    update

---
+++ order-service/src/main/java/com/example/Application8080.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application8080 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application8080.class, args);
+    }
+
+}
---
+++ order-service/src/main/java/com/example/service/impl/OrderServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.service.impl;
+
+import com.example.entity.Order;
+import com.example.mapper.OrderMapper;
+import com.example.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @Override
+    public Order queryOrderById(Long orderId) {
+        // 1.查询订单
+        Order order = orderMapper.findById(orderId);
+        // 4.返回
+        return order;
+    }
+
+}
